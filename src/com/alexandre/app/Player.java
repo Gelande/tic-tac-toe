2d9352963d4683eb6dd8f28ee6c01a9b0f8195fa
Alexandre Gelande <agelande@gmail.com>
2023-07-20 21:43:52 -0300

    Concluído a classe Player.

--- src/com/alexandre/app/Player.java
+++ src/com/alexandre/app/Player.java
@@ -1,4 +1,40 @@
 package com.alexandre.app;
 
+import com.alexandre.io.Console;
+
+/**
+ * Representa um jogador
+ * */
+@SuppressWarnings("all")
 public class Player {
+    /**
+     * Nome do jogador
+     * */
+    private String nome;
+    /**
+     * Simbolo do jogador: 'X' ou 'O'
+     * */
+    private char simbolo;
+/**
+ * Construtor do jogador
+ * @param nome
+ * @param simbolo
+ * */
+    public Player(String nome, char simbolo) {
+        this.nome = nome;
+        this.simbolo = simbolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+    public Move getMove() throws InvalidMoveException {
+        String str = Console.readString();
+        return new Move(str);
+    }
 }
